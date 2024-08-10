cd896469cd78bededa8549cf0aa8ca4e44ae3f01
soumyadipsarkar2 <somshich@gmsil.com>
2024-08-10 19:44:46 +0530

    fix-minor-proddetailspage

--- components/products/AddToCart.tsx
+++ components/products/AddToCart.tsx
@@ -19,15 +19,28 @@ export default function AddToCart({ item }: { item: OrderItem }) {
     }
     return existItem ? (
        
-     <div className="card flex flex-row justify-content-centre">
-        <button className="btn" type="button" onClick={()=>decrease(existItem)}>
-          -
-        </button>
-        <span className="px-2">{existItem.qty}</span>
-        <button className="btn" type="button" onClick={() => increase(existItem)}>
-          +
-        </button>
-      </div>
+      <button
+      className="block w-full px-6 py-2 mx-auto my-8 text-white uppercase bg-black border border-black rounded-full flex items-center justify-center space-x-2"
+      type="button"
+     
+    >
+      <button
+        className="bg-gray-700 text-white w-10 h-10 flex items-center justify-center rounded-full"
+        type="button"
+        onClick={()=>decrease(existItem)}
+      >
+        -
+      </button>
+      <span className="text-xl">{existItem.qty}</span>
+      <button
+        className="bg-gray-700 text-white w-10 h-10 flex items-center justify-center rounded-full"
+        type="button"
+       onClick={() => increase(existItem)}
+      >
+        +
+      </button>
+    </button>
+    
        
      
     ) : (
